23602fefd4e27e3f6330e91a4cc0e2fa345e2fdd
sigiltenebrae <ck6202@rit.edu>
2022-06-15 16:59:03 -0400

    even more project additions

--- src/project.ts
+++ src/project.ts
@@ -14,8 +14,8 @@ export const projects = [
     name: 'Image Filter',
     summary: 'An image manipulation application written in C++.',
     description:
-      'A C++ application using the OpenCV and Magick++ libraries to manipulate image files. Currently capable of' +
-      ' neon-laser outline, laser-etched wood, and glass-cut effects.',
+      '<p>A C++ application using the OpenCV and Magick++ libraries to manipulate image files. Currently capable of' +
+      ' neon-laser outline, laser-etched wood, and glass-cut effects.</p>',
     thumbnail: 'assets/thumbnails/filter_thumb.png',
     images: [
       'assets/projects/filter/Darth_Vader.jpg',
@@ -29,9 +29,9 @@ export const projects = [
     id: 2,
     name: 'Custom DBMS',
     summary: 'A miniature SQL clone written in Java',
-    description: 'An ACID compliant DBMS capable of handling variable-length records and B+ Tree parsing. ' +
+    description: '<p>An ACID compliant DBMS capable of handling variable-length records and B+ Tree parsing. ' +
       'Implements a page buffer for faster access time and writes all data to disk for persistent storage. Written in ' +
-      'Java.',
+      'Java.</p>',
     thumbnail: '',
     images: [],
     source: ''
@@ -40,10 +40,10 @@ export const projects = [
     id: 3,
     name: 'Automated Garden',
     summary: 'Garden management utility on a Raspberry Pi',
-    description: 'A python script running on a Raspberry Pi that uses sensors to determine when to automatically water ' +
+    description: '<p>A python script running on a Raspberry Pi that uses sensors to determine when to automatically water ' +
       'plants. Connects to OpenWeather API to get the weekly weather forecast and cancel hydration if rain is ' +
-      'predicted.',
-    thumbnail: '',
+      'predicted.</p>',
+    thumbnail: 'assets/thumbnails/garden_thumb.png',
     images: [],
     source: 'https://github.com/sigiltenebrae/AutoGarden'
   },
@@ -51,8 +51,8 @@ export const projects = [
     id: 4,
     name: 'Home Lab',
     summary: 'A home server running Proxmox.',
-    description: 'A Dell r710 running Proxmox hosting 14 VMs and over 30 Docker containers. Routes home internet using pfSense. ' +
-      'Hosts a BitWarden password manager, BookStacks wiki, NextCloud storage, and a set of DBMS\'s. ',
+    description: '<p>A Dell r710 running Proxmox hosting 14 VMs and over 30 Docker containers. Routes home internet using pfSense. ' +
+      'Hosts a BitWarden password manager, BookStacks wiki, NextCloud storage, and a set of DBMS\'s.</p>',
     thumbnail: 'assets/thumbnails/proxmox_thumbnail.png',
     images: [
       'assets/projects/homelab/proxmox_screen.png',
@@ -64,26 +64,45 @@ export const projects = [
     id: 5,
     name: '3D Printers',
     summary: 'Machines that print in 3D',
-    description: '',
-    thumbnail: '',
-    images: [],
+    description: '<p>3D Printers fully customized: </p>' +
+      '<ul>' +
+      '<li>An Ender 5 Mercury One conversion to CoreXY running Klipper</li>' +
+      '<li>An Ender 3 Dual-Z Linear Rail Mod running Klipper</li>' +
+      '<li>An Ender 5 dual extruder mod running Marlin</li>' +
+      '<li>A Qidi X-Smart with glass bed mod and custom cooling ducts</li>' +
+      '</ul>',
+    thumbnail: 'assets/thumbnails/printer_thumb.png',
+    images: [
+      'assets/projects/printer/merc_one_enclosed.jpg',
+      'assets/projects/printer/ender_3.jpg',
+      'assets/projects/printer/ender_5.jpg',
+      'assets/projects/printer/qidi.jpg',
+    ],
     source: ''
   },
   {
     id: 6,
     name: 'CAD Projects',
     summary: 'Misc Designs',
-    description: '',
-    thumbnail: '',
-    images: [],
+    description: '<p>Just a quick look at some of my designs. Most are upgrades for my printers, or ' +
+      'mods on others\' upgrades. For more information, check out my Thingiverse page <a href="https://www.thingiverse.com/sigiltenebrae/designs">here</a>.</p>',
+    thumbnail: 'assets/thumbnails/cad_thumb.png',
+    images: [
+      'assets/projects/cad/deckbox.png',
+      'assets/projects/cad/e3tense.png',
+      'assets/projects/cad/Hydra_Sherpa.png',
+      'assets/projects/cad/enclosure.png',
+      'assets/projects/cad/qidi_hydra.png',
+      'assets/projects/cad/x_smart.png',
+    ],
     source: ''
   },
   {
     id: 7,
     name: 'M.A.M.E.',
     summary: 'Multiple Arcade Machine Emulator',
-    description: 'A Raspberry Pi 3 based arcade emulator running RetroPie, with custom side panels, housing, ' +
-      'and splash screen. Wired in functional buttons and joysticks, with USB controller support as well.',
+    description: '<p>A Raspberry Pi 3 based arcade emulator running RetroPie, with custom side panels, housing, ' +
+      'and splash screen. Wired in functional buttons and joysticks, with USB controller support as well.</p>',
     thumbnail: 'assets/thumbnails/mame_thumb.png',
     images: ['assets/projects/mame/mame.png'],
     source: ''
@@ -91,10 +110,10 @@ export const projects = [
   {
     id: 8,
     name: 'Angular WebApp',
-    summary: 'Node.JS & Angular Web Server',
-    description: '',
-    thumbnail: '',
+    summary: 'Node.js & Angular Web Server',
+    description: '<p>A node web server using the angular framework hosting my personal website.</p>',
+    thumbnail: 'assets/thumbnails/web_thumb.png',
     images: [],
-    source: ''
+    source: 'https://github.com/sigiltenebrae/ckobb_angular'
   },
 ];
